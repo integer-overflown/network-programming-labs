15e0645fc32180a176ea2cf9611a7214fc4d090d
max-khm <maksym.khomenko@nure.ua>
2024-03-11 19:51:23 +0200

    Add basic axum code

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+use tracing::info;
+
+const ADDRESS: &str = "0.0.0.0:3000";
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind(ADDRESS).await.unwrap();
+    info!("Listening on {ADDRESS}");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World"
 }
